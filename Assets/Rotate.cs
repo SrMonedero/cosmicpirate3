054ba4fe32785e656c1d79f0a20181e8f6aec250
Quique Alvarez <quiquealvarezmonedero@gmail.com>
2017-01-20 16:30:42 +0100

    Add first rotation version

---
+++ Assets/Rotate.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+	
+	void Start () {
+		
+	}
+
+	void Update () {
+		float horizontal = Input.GetAxisRaw ("Horizontal");
+		transform.Rotate (new Vector3 (0, 0, horizontal));
+	}
+}
