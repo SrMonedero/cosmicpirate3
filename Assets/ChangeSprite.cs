2eef4c777fcd446b56a08f08326b201e5e04cd8f
Quique Alvarez <quiquealvarezmonedero@gmail.com>
2017-01-20 18:18:28 +0100

    Add directional sprites to player

---
+++ Assets/ChangeSprite.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeSprite : MonoBehaviour {
+	void Update () {
+		float horizontal = Input.GetAxisRaw ("Horizontal");
+		if (horizontal != 0) {
+			if (horizontal > 0) {
+				transform.eulerAngles = new Vector3 (0, 180f, 0);
+			} else {
+				transform.eulerAngles = Vector3.zero;
+			}
+		}
+	}
+}
