8897666b90eaa04caba049a6c19020a9b916be30
Quique Alvarez <quiquealvarezmonedero@gmail.com>
2017-01-20 17:43:04 +0100

    Add ship rotation

---
+++ Assets/AutoRotate.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoRotate : MonoBehaviour {
+	public float speed = 0.01f;
+	public float distanceFactorSpeed = 1f;
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		float horizontal = Input.GetAxisRaw ("Horizontal");
+		transform.Rotate (0,0,horizontal * distanceFactorSpeed + speed * Time.deltaTime);	
+	}
+}
--- Assets/Rotate.cs
+++ Assets/Rotate.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Rotate : MonoBehaviour {
-	
+
+	public float speed = 1;
+
 	void Start () {
 		
 	}
 
 	void Update () {
 		float horizontal = Input.GetAxisRaw ("Horizontal");
-		transform.Rotate (new Vector3 (0, 0, horizontal));
+		transform.Rotate (new Vector3 (0, 0, horizontal * speed));
 	}
 }
