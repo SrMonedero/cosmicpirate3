e9fbec3fb4c6c1f4b4a642ef30ce806c3e96382f
Quique <quiquealvarezmonedero@gmail.com>
2017-01-21 11:00:01 +0100

    Fix continuous jumping

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -6,10 +6,15 @@ public class Player : MonoBehaviour {
 	private bool jumping = false;
 	private float jumpEnergy = 300;
 	private float jumpEnergyLoss = 100;
+	private bool lastVerticalAxisInUse = false;
 
 	void Update () {
+		UpdateSprite ();
+		UpdateJump ();
+	}
+
+	private void UpdateSprite() {
 		float horizontal = Input.GetAxisRaw ("Horizontal");
-		float vertical = Input.GetAxis ("Vertical");
 		if (horizontal != 0) {
 			if (horizontal > 0) {
 				transform.eulerAngles = new Vector3 (0, 180f, 0);
@@ -17,17 +22,22 @@ public class Player : MonoBehaviour {
 				transform.eulerAngles = Vector3.zero;
 			}
 		}
-		if (jumpEnergy > 0 && vertical > 0) {
+	}
+
+	private void UpdateJump(){
+		float vertical = Input.GetAxis ("Vertical");
+		bool verticalAxisInUse = Input.GetAxisRaw ("Vertical") != 0;
+		if (((verticalAxisInUse && !lastVerticalAxisInUse) || jumping) && jumpEnergy > 0 && vertical > 0) {
 			jumping = true;
 			Rigidbody2D body = GetComponent<Rigidbody2D> ();
 			jumpEnergy -= jumpEnergyLoss;
 			body.AddForce (new Vector2 (0, jumpEnergyLoss));
 		}
+		lastVerticalAxisInUse = verticalAxisInUse;
 	}
 
 	void OnCollisionEnter2D(Collision2D collision) {
 		jumping = false;
 		jumpEnergy = 200;
-		Debug.Log ("collision");
 	}
 }
